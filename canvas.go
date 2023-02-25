00d0c4212c309b04762504e8a5e42cc330d17e81
Chris Kim <chriskim06@users.noreply.github.com>
2023-02-25 23:14:57 +0000

    qwerty

--- canvas.go
+++ canvas.go
@@ -21,7 +21,11 @@ type Cell struct {
 	color AnsiColor
 }
 
+// String returns the cell's rune wrapped in the color escape strings
 func (c Cell) String() string {
+	if c.Rune == 0 {
+		return color(" ", c.color)
+	}
 	return color(string(c.Rune), c.color)
 }
 
@@ -31,9 +35,12 @@ type Canvas struct {
 	LabelColor AnsiColor
 	AxisColor  AnsiColor
 
+	// the bounds of the canvas
+	area image.Rectangle
+
 	// a map of the entire braille grid
-	area   image.Rectangle
 	points map[image.Point]Cell
+
 	labels []string
 	offset int
 }
@@ -41,21 +48,31 @@ type Canvas struct {
 // Make a new canvas
 func NewCanvas(width, height int) Canvas {
 	c := Canvas{
-		AxisColor: Default,
+		AxisColor:  Default,
 		LabelColor: Default,
 		LineColors: []AnsiColor{},
-		area:   image.Rect(0, 0, width-1, height-1),
-		points: make(map[image.Point]Cell),
-		labels: []string{},
+		area:       image.Rect(0, 0, width-1, height-1),
+		points:     make(map[image.Point]Cell),
+		labels:     []string{},
 	}
 	return c
 }
 
+func (c *Canvas) clear() {
+	c.points = make(map[image.Point]Cell)
+	c.labels = []string{}
+}
+
+// Plot takes a list of data points to graph
+// each inner slice represents a different line
 func (c *Canvas) Plot(data [][]float64) string {
-	_, maxDataPoint := GetMinMaxFloat64From2dSlice(data)
+	c.clear()
+	maxDataPoint := getMaxFloat64From2dSlice(data)
 	lenMaxDataPoint := len(fmt.Sprintf("%.2f", maxDataPoint))
 	c.offset = lenMaxDataPoint + 3
 	verticalScale := maxDataPoint / float64(c.area.Dy())
+
+	// setup y-axis labels
 	for i := 0; i < c.area.Dy(); i++ {
 		val := fmt.Sprintf("%.2f", float64(i)*verticalScale)
 		padding := ""
@@ -65,12 +82,14 @@ func (c *Canvas) Plot(data [][]float64) string {
 		label := fmt.Sprintf("%s%s %s ", padding, color(val, c.LabelColor), color("┤", c.AxisColor))
 		c.labels = append(c.labels, label)
 	}
+
+	// plot the data
 	for i, line := range data {
 		previousHeight := int((line[1] / maxDataPoint) * float64(c.area.Dy()-1))
 		for j, val := range line[1:] {
 			height := int((val / maxDataPoint) * float64(c.area.Dy()-1))
 			startX := c.area.Min.X + c.offset
-			c.SetLine(
+			c.setLine(
 				image.Pt(
 					(startX+j)*2,
 					(c.area.Max.Y-previousHeight-1)*4,
@@ -84,35 +103,34 @@ func (c *Canvas) Plot(data [][]float64) string {
 			previousHeight = height
 		}
 	}
-	return c.String()
+	return c.string()
 }
 
-func (c Canvas) String() string {
+func (c Canvas) string() string {
 	var b strings.Builder
-	// max := 0
-	cells := c.GetCells()
+	cells := c.getCells()
 	for row := c.area.Dy() - 1; row >= 0; row-- {
 		label := c.labels[row]
 		b.WriteString(label)
 		for col := c.offset; col < c.area.Dx(); col++ {
-			r := " "
-			if cell, ok := cells[image.Pt(col, row)]; ok {
-				r = cell.String()
-			}
-			b.WriteString(r)
+			b.WriteString(cells[image.Pt(col, row)].String())
 		}
 		b.WriteRune('\n')
 	}
 
 	// this is so that the x-axis can start at the y-axis line
 	offset := c.offset - 2
-	b.WriteString(strings.Repeat(" ", offset))
-	b.WriteString(color(string('╰'), c.AxisColor))
-	b.WriteString(color(strings.Repeat("─", c.area.Dx()-offset), c.AxisColor))
+	xaxis := fmt.Sprintf(
+		"%s%s%s",
+		strings.Repeat(" ", offset),
+		color(string('╰'), c.AxisColor),
+		color(strings.Repeat("─", c.area.Dx()-offset), c.AxisColor),
+	)
+	b.WriteString(xaxis)
 	return b.String()
 }
 
-func (c *Canvas) SetPoint(p image.Point, color AnsiColor) {
+func (c *Canvas) setPoint(p image.Point, color AnsiColor) {
 	point := image.Pt(p.X/2, p.Y/4)
 	c.points[point] = Cell{
 		c.points[point].Rune | BRAILLE[p.Y%4][p.X%2],
@@ -120,46 +138,16 @@ func (c *Canvas) SetPoint(p image.Point, color AnsiColor) {
 	}
 }
 
-func (c *Canvas) SetLine(p0, p1 image.Point, color AnsiColor) {
+func (c *Canvas) setLine(p0, p1 image.Point, color AnsiColor) {
 	for _, p := range line(p0, p1) {
-		c.SetPoint(p, color)
+		c.setPoint(p, color)
 	}
 }
 
-func (c *Canvas) GetCells() map[image.Point]Cell {
+func (c *Canvas) getCells() map[image.Point]Cell {
 	points := make(map[image.Point]Cell)
 	for point, cell := range c.points {
 		points[point] = Cell{cell.Rune + BRAILLE_OFFSET, cell.color}
 	}
-	return points
-}
-
-func line(p0, p1 image.Point) []image.Point {
-	points := []image.Point{}
-
-	leftPoint, rightPoint := p0, p1
-	if leftPoint.X > rightPoint.X {
-		leftPoint, rightPoint = rightPoint, leftPoint
-	}
-
-	xDistance := absInt(leftPoint.X - rightPoint.X)
-	yDistance := absInt(leftPoint.Y - rightPoint.Y)
-	slope := float64(yDistance) / float64(xDistance)
-	slopeSign := 1
-	if rightPoint.Y < leftPoint.Y {
-		slopeSign = -1
-	}
-
-	targetYCoordinate := float64(leftPoint.Y)
-	currentYCoordinate := leftPoint.Y
-	for i := leftPoint.X; i < rightPoint.X; i++ {
-		points = append(points, image.Pt(i, currentYCoordinate))
-		targetYCoordinate += (slope * float64(slopeSign))
-		for currentYCoordinate != int(targetYCoordinate) {
-			points = append(points, image.Pt(i, currentYCoordinate))
-			currentYCoordinate += slopeSign
-		}
-	}
-
 	return points
 }
--- utils.go
+++ utils.go
@@ -2,62 +2,53 @@ package drawille
 
 import (
 	"fmt"
-	"math"
+	"image"
 )
 
-// dots:
-//    8x8 cells, 4x2 chars
-//    ,___,___,___,___,
-//    |1 4|1 4|1 4|1 4|
-//    |2 5|2 5|2 5|2 5|
-//    |3 6|3 6|3 6|3 6|
-//    |7 8|7 8|7 8|7 8|
-//    -----------------
-//    |1 4|1 4|1 4|1 4|
-//    |2 5|2 5|2 5|2 5|
-//    |3 6|3 6|3 6|3 6|
-//    |7 8|7 8|7 8|7 8|
-//    `````````````````
-/*
-var pixel_map = [4][2]int{
-	{0x1, 0x8},
-	{0x2, 0x10},
-	{0x4, 0x20},
-	{0x40, 0x80},
-}
-
-// Braille chars start at 0x2800
-var braille_char_offset = 0x2800
-
-func getPixel(y, x int) int {
-	var cy, cx int
-	if y >= 0 {
-		cy = y % 4
-	} else {
-		cy = 3 + ((y + 1) % 4)
-	}
-	if x >= 0 {
-		cx = x % 2
-	} else {
-		cx = 1 + ((x + 1) % 2)
-	}
-	return pixel_map[cy][cx]
-}
-*/
-
-func GetMinMaxFloat64From2dSlice(slices [][]float64) (float64, float64) {
-	min, max := math.Inf(1), math.Inf(-1)
+func getMaxFloat64From2dSlice(slices [][]float64) float64 {
+	var max float64
 	for _, slice := range slices {
 		for _, val := range slice {
 			if val > max {
 				max = val
 			}
-			if val < min {
-				min = val
-			}
 		}
 	}
-	return min, max
+	return max
+}
+
+func color(s string, c AnsiColor) string {
+	return fmt.Sprintf("%s%s%s", c.String(), s, Default)
+}
+
+func line(p0, p1 image.Point) []image.Point {
+	points := []image.Point{}
+
+	leftPoint, rightPoint := p0, p1
+	if leftPoint.X > rightPoint.X {
+		leftPoint, rightPoint = rightPoint, leftPoint
+	}
+
+	xDistance := absInt(leftPoint.X - rightPoint.X)
+	yDistance := absInt(leftPoint.Y - rightPoint.Y)
+	slope := float64(yDistance) / float64(xDistance)
+	slopeSign := 1
+	if rightPoint.Y < leftPoint.Y {
+		slopeSign = -1
+	}
+
+	targetYCoordinate := float64(leftPoint.Y)
+	currentYCoordinate := leftPoint.Y
+	for i := leftPoint.X; i < rightPoint.X; i++ {
+		points = append(points, image.Pt(i, currentYCoordinate))
+		targetYCoordinate += (slope * float64(slopeSign))
+		for currentYCoordinate != int(targetYCoordinate) {
+			points = append(points, image.Pt(i, currentYCoordinate))
+			currentYCoordinate += slopeSign
+		}
+	}
+
+	return points
 }
 
 func absInt(x int) int {
@@ -65,8 +56,4 @@ func absInt(x int) int {
 		return x
 	}
 	return -x
-}
-
-func color(s string, c AnsiColor) string {
-	return fmt.Sprintf("%s%s%s", c.String(), s, Default)
 }
